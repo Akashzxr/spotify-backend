de1a4f309207e37b322c2ebdb9b13e4d4bbf2e06
akashks <akashks8855@gmail.com>
2024-12-26 17:13:52 +0530

    updated

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -23,29 +23,34 @@ module.exports.Signup = async (req, res, next) => {
   }
 };
 
-
 module.exports.Login = async (req, res, next) => {
-    try {
-      const { email, password } = req.body;
-      if(!email || !password ){
-        return res.json({message:'All fields are required'})
-      }
-      const userDetails = await user.findOne({ email });
-      if(!user){
-        return res.json({message:'Incorrect password or email' }) 
-      }
-      const auth = await bcrypt.compare(password,userDetails.password)
-      if (!auth) {
-        return res.json({message:'Incorrect password or email' }) 
-      }
-       const token = await createSecretToken(userDetails._id);
-       res.cookie("token", token, {
-         withCredentials: true,
-         httpOnly: false,
-       });
-       res.status(201).json({ message: "User logged in successfully", success: true });
-       next()
-    } catch (error) {
-      console.error(error);
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ message: "All fields are required" });
     }
+    const userDetails = await user.findOne({ email });
+    if (!user) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+    const auth = await bcrypt.compare(password, userDetails.password);
+    if (!auth) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+    const token = await createSecretToken(userDetails._id);
+
+    res
+      .cookie("token", token, {
+        withCredentials: true,
+        httpOnly: false,
+      })
+      .then(
+        res
+          .status(201)
+          .json({ message: "User logged in successfully", success: true })
+      );
+    next();
+  } catch (error) {
+    console.error(error);
   }
+};
