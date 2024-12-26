d4e5d4d114271d8d76b0f4186dd161d2e132b31b
akashks <akashks8855@gmail.com>
2024-12-26 22:36:49 +0530

    updated

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -38,17 +38,17 @@ module.exports.Login = async (req, res, next) => {
       return res.json({ message: "Incorrect password or email" });
     }
     const token = await createSecretToken(userDetails._id);
-
+    console.log("token in login:"+token);
+    
     res
       .cookie("token", token, {
         withCredentials: true,
         httpOnly: false,
       })
-      .then(
+      
         res
           .status(201)
-          .json({ message: "User logged in successfully", success: true })
-      );
+          .json({ message: "User logged in successfully", success: true,token:token });
     next();
   } catch (error) {
     console.error(error);
